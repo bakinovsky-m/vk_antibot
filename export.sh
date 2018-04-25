dcf74dd2e32ed520b0e0d932307e658bd2a2f4a9
bakinovsky-m <makbak@ya.ru>
2018-04-25 13:40:15 +0300

    шо-то новенькое

--- export.sh
+++ export.sh
@@ -1,4 +1,8 @@
-sqlite3 antibot.db -header -csv "select id, (first_name || ' ' || last_name) as Label from accounts" > nodes.csv
-sqlite3 antibot.db -header -csv "select source, target from relations" > edges.csv
-sqlite3 antibot.db -header -csv "select accounts.id, (accounts.first_name || ' ' || accounts.last_name) as Label, groups.name as group_name from accounts join accounts_groups on accounts_groups.account_id = accounts.id join groups on accounts_groups.group_id = groups.id" > group.csv
+sqlite3 antibot.db -header -csv "select id, (first_name || ' ' || last_name) as Label from accounts where affected = 1" > nodes.csv
+# sqlite3 antibot.db -header -csv "select source, target from relations" > edges.csv
+# sqlite3 antibot.db -header -csv "select accounts.id, (accounts.first_name || ' ' || accounts.last_name) as Label, groups.name as group_name from accounts join accounts_groups on accounts_groups.account_id = accounts.id join groups on accounts_groups.group_id = groups.id" > group.csv
 # select accounts.id, accounts.first_name, accounts.last_name, groups.name from accounts join accounts_groups on accounts_groups.account_id = accounts.id join groups on accounts_groups.group_id = groups.id
+# sqlite3 antibot.db -header -csv "select relations.source, relations.target from relations join accounts on "
+
+# select * from relations join accounts on relations.source = accounts.id join accounts as accs on relations.target = accs.id where (accounts.affected = 1) and (accs.affected = 1);
+sqlite3 antibot.db -header -csv "select relations.source as source, relations.target as target from relations join accounts on relations.source = accounts.id join accounts as accs on relations.target = accs.id where (accounts.affected = 1) and (accs.affected = 1);" > edges.csv
--- vk_antibot.py
+++ vk_antibot.py
@@ -3,13 +3,17 @@ import sqlite3
 from pathlib import Path
 
 DB_FILENAME = 'antibot.db'
+BOT_USER_ID = 0
+API = None
+BOT_FRIENDS = None
 
 def create_db(sql_cursor):
 	c.execute('''
 		CREATE TABLE if not exists accounts (
 		id int primary key unique,
 		first_name text, 
-		last_name text
+		last_name text,
+		affected int
 		)
 		''')
 	c.execute('''
@@ -46,6 +50,17 @@ def get_creds():
 	password = f.readline()
 	return (login, password)
 
+def is_affected(a):
+	# print('Checking if {} {} is affected by bot'.format(a['first_name'], a['last_name']), end=' ', flush=True)
+	# a_followers = API.users.getFollowers(user_id=a['id'])
+	# if a['id'] in BOT_FRIENDS['items'] or BOT_USER_ID in a_followers['items']:
+	if a['id'] in BOT_FRIENDS['items']:
+		# print('YES')
+		return True
+	else:
+		# print('NO')
+		return False
+
 db_file = Path(DB_FILENAME)
 if db_file.is_file():
 	db_file.unlink()
@@ -58,43 +73,79 @@ login, password = get_creds()
 print('Authenticate, may take a while')
 session = vk.VkApi(login=login, password=password)
 session.auth()
-api = session.get_api()
+API = session.get_api()
 
-print('''Getting Sveta's id''')
-# target_user = api.users.get(user_ids='svetlanka_lashchuk', v='5.21')
-target_user = api.users.get(user_ids='nadia677', v='5.21')
+print('''Getting target's and bot's ids''')
+users_request = API.users.get(user_ids='svetlanka_lashchuk, nadia677')
+target_user = users_request[0]
+BOT_USER_ID = users_request[1]['id']
+BOT_FRIENDS = API.friends.get(user_id=BOT_USER_ID)
 
-sv_id = target_user[0]['id']
-
-print('''Getting friends''')
-friends = api.friends.get(user_id=sv_id, fields='first_name, last_name')
+print('''Getting target friends''')
+friends = API.friends.get(user_id=target_user['id'], fields='first_name, last_name')
 friends = friends['items']
 friends = [f for f in friends if f['first_name'] != 'DELETED' and 'deactivated' not in f.keys()]
-friends.append(target_user[0])
-# for f in friends:
-# 	print(f)
 
+## ALARM !!!!!
+
+# friends = friends[:10]
+
+## ALARM !!!!!
+
+friends.append(target_user)
+
+counter1 = 1
 for f in friends:
-	print('\nWorking on {} {}'.format(f['first_name'], f['last_name']))
-	tmp = (f['id'], f['first_name'], f['last_name'])
-	c.execute('INSERT or ignore INTO accounts VALUES (?, ?, ?)', tmp)
+	print('\nWorking on {} {}. {}/{}'.format(f['first_name'], f['last_name'], counter1, len(friends)))
+	tmp = (f['id'], f['first_name'], f['last_name'], is_affected(f))
+	c.execute('INSERT or ignore INTO accounts VALUES (?, ?, ?, ?)', tmp)
 
 	print('''\tGetting friends of friend''')
-	ff = api.friends.get(user_id=f['id'], fields='first_name, last_name')['items']
-	ff_to_acc = [(fff['id'], fff['first_name'], fff['last_name']) for fff in ff if f['first_name'] != 'DELETED' and 'deactivated' not in f.keys()]
-	ff_to_rel = [(f['id'], fff['id']) for fff in ff if f['first_name'] != 'DELETED' and 'deactivated' not in f.keys()]
-	c.executemany('''INSERT or ignore INTO accounts values (?, ?, ?)''', ff_to_acc)
+	friends_of_friends = API.friends.get(user_id=f['id'], fields='first_name, last_name')['items']
+	friends_of_friends = [ff for ff in friends_of_friends if 'deactivated' not in ff.keys()]
+	ff_to_acc = []
+	for ff in friends_of_friends:
+		tmp = (ff['id'], ff['first_name'], ff['last_name'], is_affected(ff))
+
+		ff_to_acc.append(tmp)
+
+	ff_to_rel = [(f['id'], fff['id']) for fff in friends_of_friends if 'deactivated' not in f.keys()]
+	c.executemany('''INSERT or ignore INTO accounts values (?, ?, ?, ?)''', ff_to_acc)
 	c.executemany('INSERT INTO relations(source, target) values (?, ?)', ff_to_rel)
 
-	print('''\tWorking on groups''')
-	f_groups = api.groups.get(user_id=f['id'], extended=1)['items']
-	f_groups_to_sql = [(g['id'], g['name'], g['screen_name']) for g in f_groups]
-	c.executemany('''INSERT OR IGNORE INTO groups(id, name, screen_name) values (?, ?, ?)''', f_groups_to_sql)
+	# print('''\tWorking on groups''')
+	# f_groups = API.groups.get(user_id=f['id'], extended=1)['items']
+	# f_groups_to_sql = [(g['id'], g['name'], g['screen_name']) for g in f_groups]
+	# c.executemany('''INSERT OR IGNORE INTO groups(id, name, screen_name) values (?, ?, ?)''', f_groups_to_sql)
 
-	f_groups_ids = [(f['id'], g['id']) for g in f_groups]
-	# print(f_groups_ids)
-	c.executemany('''INSERT INTO accounts_groups(account_id, group_id) values (?, ?)''', f_groups_ids)
+	# f_groups_ids = [(f['id'], g['id']) for g in f_groups]
+	# c.executemany('''INSERT INTO accounts_groups(account_id, group_id) values (?, ?)''', f_groups_ids)
+
+
+
 	conn.commit()
+	counter1 += 1
 
-# conn.commit()
+counter2 = 1
+for friend in friends:
+	print('Building relations of {} {}. {}/{}'.format(friend['first_name'], friend['last_name'], counter2, len(friends)))
+	ff = API.friends.get(user_id=friend['id'])
+	# c.execute('''SELECT id from ''')
+	ff = ff['items']
+
+	# to_db = [(friend['id'], f_id) for f_id in ff]
+
+	c.execute('''SELECT id from accounts''')
+	ids = c.fetchall()
+	ids = [idd[0] for idd in ids]
+
+	# print(ids)
+	# import sys; sys.quit()
+	to_db = [(friend['id'], idd) for idd in ids if idd in ff]
+
+
+	c.executemany('''INSERT into relations(source, target) values (?, ?)''', to_db)
+	conn.commit()
+	counter2 += 1
+
 conn.close()
